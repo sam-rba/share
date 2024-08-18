9432f7d1c143c426ab011770431827b9c055b7e9
Sam Anthony <sam@samanthony.xyz>
2024-08-18 18:01:24 -0400

    test ConstSlice

---
+++ slice_test.go
@@ -0,0 +1,32 @@
+package share_test
+
+import (
+	"testing"
+
+	"git.samanthony.xyz/share"
+)
+
+func TestConstSlice(t *testing.T) {
+	orig := []string{"foo", "bar", "baz"}
+	shared := share.NewConstSlice(orig)
+	verifySame(shared, orig, t)
+	go func() {
+		defer shared.Close()
+		verifySame(shared, orig, t)
+	}()
+}
+
+func verifySame[T comparable](cs share.ConstSlice[T], s []T, t *testing.T) {
+	i := 0
+	for elem := range cs.Elems() {
+		if i < len(s) {
+			if elem != s[i] {
+				t.Errorf("ConstSlice[%d] = %v; expected %v", i, elem, s[i])
+			}
+		}
+		i++
+	}
+	if i != len(s) {
+		t.Errorf("ConstSlice.Elems() returned %d elements; expected %d", i, len(s))
+	}
+}
